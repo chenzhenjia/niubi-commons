5e7b7612d70d78139a3569fb4761a6ffa02a9e25
chenzhenjia <chzhenjia@gmail.com>
2020-04-17 21:48:24 +0800

    build(gradle): 升级spring boot 的版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,8 @@ plugins {
     java
     `java-library`
     id("org.jetbrains.kotlin.jvm") version "1.3.70" apply false
-    id("org.springframework.boot") version "2.2.1.RELEASE" apply false
-    id("io.spring.dependency-management") version "1.0.8.RELEASE" apply false
+    id("org.springframework.boot") version "2.2.6.RELEASE" apply false
+    id("io.spring.dependency-management") version "1.0.9.RELEASE" apply false
 }
 
 allprojects {
